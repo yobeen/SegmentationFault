f40c05267b6cc33e8c8afae72b5fced1eabdacf5
yobeen <kirillgrish@gmail.com>
2015-06-19 15:26:04 -0700

    Add index test, move answers order test to answer_spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Answer, type: :model do
   it { should validate_length_of(:content).is_at_most(15000) } 
 
   it { should belong_to(:question) } 
+  
+  it { have_db_index(:created_at) }
+  
+  let!(:answer1) { FactoryGirl.create(:answer, created_at: 2.days.ago) }
+  let!(:answer2) { FactoryGirl.create(:answer, created_at: 1.day.ago) }
+  let!(:answer3) { FactoryGirl.create(:answer, created_at: 4.days.ago) }
+
+  it 'answers ordered by creation date descending' do
+    expect(Answer.pluck(:id)).to eq [answer2, answer1, answer3].map{ |a| a.id }
+  end
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,13 +8,5 @@ RSpec.describe Question, type: :model do
   it { should validate_length_of(:title).is_at_most(250) } 
 
   it { should have_many(:answers).dependent(:destroy) }
-  
-  let!(:answer1) { FactoryGirl.create(:answer, created_at: 2.days.ago); }
-  let!(:answer2) { FactoryGirl.create(:answer, created_at: 1.day.ago); }
-  let!(:answer3) { FactoryGirl.create(:answer, created_at: 4.days.ago); }
-
-  it 'answers ordered by creation date descending' do
-    expect(Answer.pluck(:id)).to eq [answer2, answer1, answer3].map{ |a| a.id }
-  end
 
 end
