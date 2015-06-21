31e08a651edbdb14e721f9d6caf2ea9ea91e7de7
yobeen <kirillgrish@gmail.com>
2015-06-21 14:11:18 -0700

    fixed factorygirl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,10 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'rubygems'
+require 'factory_girl_rails'
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
