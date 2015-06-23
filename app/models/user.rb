7685e3385d417a81c75b7378774921a8d65da91e
yobeen <kirillgrish@gmail.com>
2015-06-23 11:20:27 -0700

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :questions, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :answers, dependent: :destroy
-
-  validates :name, presence: true, length: { maximum: 50 }
+  
+  user_regex = /\A[a-zA-z0-9][a-zA-z0-9 _\-.']/
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }, format: { with: user_regex }
 end
